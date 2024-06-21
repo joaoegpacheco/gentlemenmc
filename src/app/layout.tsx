3261d03e58842c7cba188b0b91c113516304e796
João Pacheco <jao.pacheco1@gmail.com>
2024-06-21 17:23:16 -0300

    remove bootstrap css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import "./gentlemen.css";
-import "./bootstrap-icons.css";
-import "./bootstrap.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
