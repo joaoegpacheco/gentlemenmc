ec405906febd06c787ea079f930a1ea208935f05
João Pacheco <jao.pacheco1@gmail.com>
2024-06-26 14:19:42 -0300

    fix css

--- src/components/Card/page.tsx
+++ src/components/Card/page.tsx
@@ -78,7 +78,7 @@ export function CardComand() {
           sm: 2,
           md: 4,
           lg: 4,
-          xl: 6,
+          xl: 4,
           xxl: 3,
         }}
         renderItem={(item) => (
