0db4988115fa063c8c0ae9cdedfe40de7900c71b
João Pacheco <jao.pacheco1@gmail.com>
2025-02-24 21:31:59 -0300

    refact name env

--- src/hooks/use-supabase.js
+++ src/hooks/use-supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
   const supabase = createClient(
-    process.env.SUPABASE_WEB ? process.env.SUPABASE_WEB : process.env.NEXT_PUBLIC_SUBABASE_WEB,
-    process.env.SUPABASE_KEY ? process.env.SUPABASE_KEY : process.env.NEXT_PUBLIC_SUBABASE_KEY
+    process.env.NEXT_PUBLIC_SUBABASE_WEB,
+    process.env.NEXT_PUBLIC_SUBABASE_KEY
   );
 
 export default supabase;
