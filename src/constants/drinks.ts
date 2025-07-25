66088b8007b3f5f35e79dd1214e3e7eac6839525
João Pacheco <jao.pacheco1@gmail.com>
2025-07-25 16:56:35 -0300

    add news prices to guests

---
+++ src/constants/drinks.ts
@@ -0,0 +1,14 @@
+export const DRINKS_PRICES: Record<string, number> = {
+  "Chopp Pilsen": 12,
+  "Chopp Mutum": 20,
+  "Long Neck Heineken/Corona": 15,
+  "Energético": 15,
+  "Refrigerante": 6,
+  "Água": 5,
+  "Dose Gin": 10,
+  "Dose Jagermeister": 20,
+  "Dose Whiskey": 20,
+  "Dose Campari": 10,
+  "Dose Rum": 15,
+  "Carteira de Cigarro": 15,
+};
