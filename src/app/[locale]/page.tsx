46e2abd40a9e3ec2d3f93996910ee582dfef2329
João Pacheco <jao.pacheco1@gmail.com>
2026-07-03 17:48:09 -0300

    feat(auth): wrap login page with background shell layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { LoginForm } from "@/components/LoginForm/page";
+import { LoginBackground } from "@/components/LoginBackground/page";
 
 export default function Home() {
   return (
-    <LoginForm />
-  )
+    <div className="login-page-shell flex min-h-[calc(100vh-7rem)] w-full items-center justify-center">
+      <LoginBackground />
+      <LoginForm />
+    </div>
+  );
 }
